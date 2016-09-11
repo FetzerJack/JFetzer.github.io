340dd1b8b830e798acbfd3268a6916ebeb13aec3
Jack Fetzer <fetzerjack@yahoo.com>
2016-09-11 16:01:28 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,6 @@
+
+//This adds an alert when the page is loaded.
+function onLoad() {
+    //alert("Function onLoad() called")
+        document.getElementById("timestamp").innerHTML = Date(); 
+        }
