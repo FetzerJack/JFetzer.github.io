f1762001b40c79645001c6e4b20771413b4dcf9c
Jack Fetzer <fetzerjack@yahoo.com>
2018-10-04 20:26:49 -0400

    Beginning of the Overhall. Now using atom.

--- app.js
+++
@@ -1,6 +0,0 @@
-
-//This adds an alert when the page is loaded.
-function onLoad() {
-    //alert("Function onLoad() called")
-        document.getElementById("timestamp").innerHTML = Date(); 
-        }
